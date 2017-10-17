b11c7252805897526d9c275f9c8b4c001e948bce
Unknown <max.christie21@gmail.com>
2017-10-17 11:44:40 -0400

    Refactored Dictionary.c .h

--- dictionary.c
+++ dictionary.c
@@ -1,67 +1,86 @@
+#include <stdio.h>
 #include <string.h>
 #include "dictionary.h"
-#include <stdio.h>
 
+/*
+ * Form hash value for string s
+ * This produces a starting value in the dictionary array
+ */
+unsigned Hash(const char* s)
+{
+	unsigned hashVal;
 
-//form hash value for string s
-//this produces a starting value in the dictionary array
-unsigned hash(const char *s) {
-	unsigned hashval;
-	for (hashval = 0; *s != '\0'; s++)
-		hashval = *s + 31 * hashval;
-	return hashval ;
+	for (hashVal = 0; *s != '\0'; ++s) {
+		hashVal = *s + (31 * hashVal);
+	}
+
+	return hashVal;
 }
 
-DNode * lookup (DNode ** dictionary, int hash_size, const char *key) {
-	DNode * np;
-	unsigned int hashval = hash(key);
-	for (np = dictionary [hashval % hash_size]; np !=NULL; np = np->next)
-		if (strcmp (key, np->key) == 0)
-			return np; 
+DNode* LookUp(DNode** dictionary, int hashSize, const char* key)
+{
+	DNode* np;
+	unsigned int hashVal = Hash(key);
+
+	for (np = dictionary[hashVal % hashSize]; np != NULL; np = np->next) {
+		if (strCmp(key, np->key) == 0) {
+			return np;
+		}
+	}
+
 	return NULL; //not found
 }
 
-DNode * insert (DNode ** dictionary, int hash_size,  const char * key) {
-	unsigned int hashval;
-	DNode *np;
+DNode* Insert(DNode** dictionary, int hashSize, const char* key)
+{
+    DNode* np;
+	unsigned int hashVal;
 
-	if ((np = lookup (dictionary, hash_size, key)) == NULL ) { //
-		np = (DNode *) malloc (sizeof (*np));
+    if ((np = LookUp(dictionary, hashSize, key)) == NULL ) {
+		np = (DNode *)malloc(sizeof (*np));
 
-		if (np == NULL || (np->key = copystr (key)) == NULL)
+		if (np == NULL || (np->key = copyStr (key)) == NULL) {
 			return NULL;
+		}
 
-		hashval = hash (key) % hash_size;
+		hashVal = Hash(key) % hashSize;
 
-		np->next = dictionary [hashval];
-		dictionary [hashval] = np;
+		np->next = dictionary[hashVal];
+		dictionary[hashVal] = np;
 	}
+
 	return np;
 }
 
-void free_dictionary (DNode ** dictionary, int hash_size) {
+void FreeDictionary(DNode** dictionary, int hashSize)
+{
 	int i;
-	for (i=0; i<hash_size; i++) { 
-		if (dictionary [i]!=NULL) { //if there is an entry at position i
-			DNode *head = dictionary[i]; 
-			DNode *current = head;
+
+	for (i = 0; i < hashSize; ++i) {
+		if (dictionary[i] != NULL) { //if there is an entry at position i
+			DNode* head = dictionary[i];
+			DNode* current = head;
 			while (current != NULL) {
-				DNode * temp = current;
+				DNode* temp = current;
 				current = current->next;
-				free (temp);
+				free(temp);
 			}
+
 			dictionary[i] = NULL;  //BUG fix
 		}
 	}
 }
 
-char *copystr(const char *s) { /* make a duplicate of s */
-	char *p;
+char* CopyStr(const char* s)
+{
+	char* p;
 	int len = strlen(s);
 
-	p = (char *) malloc(len+1); /* +1 for ?\0? */
-	if (p != NULL)
+	p = (char *) malloc(len + 1); // +1 for ?\0
+	if (p != NULL) {
 		strncpy(p, s, len);
+	}
+
 	p[len] = '\0';
 
 	return p;
